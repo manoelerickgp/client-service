3e9ae22ab09c175d1e077484b5f6b637c8eb807d
manoelerickgp <manoel.computacao@gmail.com>
2023-09-11 23:41:31 -0300

    Commit inicial

---
+++ src/main/java/com/projeto/ClientRegister/ClientRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ClientRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegisterApplication.class, args);
+	}
+
+}
