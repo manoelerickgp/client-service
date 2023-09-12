20c68aacaafaa87ba092c39c9aa6443c1e705fed
manoelerickgp <manoel.computacao@gmail.com>
2023-09-12 13:55:59 -0300

    Adicionando metodo de buscar cliente por id

--- src/main/java/com/projeto/ClientRegister/Controllers/ClientResource.java
+++ src/main/java/com/projeto/ClientRegister/Controllers/ClientResource.java
@@ -2,11 +2,9 @@ package com.projeto.ClientRegister.Controllers;
 
 import com.projeto.ClientRegister.entities.Client;
 import com.projeto.ClientRegister.services.ClientService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,12 @@ public class ClientResource {
     public ResponseEntity<List<Client>> findAll(){
         List<Client> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Client> findById(@PathVariable Long id) {
+        Client client = service.findById(id);
+        return ResponseEntity.ok().body(client);
     }
 
 
