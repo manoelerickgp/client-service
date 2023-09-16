0273b5563916328b106bdd637407bc08473aa628
manoelerickgp <manoel.computacao@gmail.com>
2023-09-15 23:59:52 -0300

    Adicionando cliente service test

--- src/main/java/com/projeto/ClientRegister/repositories/ClientRepository.java
+++ src/main/java/com/projeto/ClientRegister/repositories/ClientRepository.java
@@ -1,7 +1,6 @@
 package com.projeto.ClientRegister.repositories;
 
 import com.projeto.ClientRegister.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
---
+++ src/main/java/com/projeto/ClientRegister/services/ClientServiceTest.java
@@ -0,0 +1,4 @@
+package com.projeto.ClientRegister.services;
+
+public class ClientServiceTest {
+}
