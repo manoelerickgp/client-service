e4f016910c773f98814a5df911518a14afbf292e
manoelerickgp <manoel.computacao@gmail.com>
2023-09-18 01:16:45 -0300

    Atualizando metodos Rest do ClientResource com Padrao Dto

--- src/main/java/com/projeto/ClientRegister/Controllers/ClientResource.java
+++ src/main/java/com/projeto/ClientRegister/Controllers/ClientResource.java
@@ -1,7 +1,9 @@
 package com.projeto.ClientRegister.Controllers;
 
+import com.projeto.ClientRegister.DTO.ClientDTO;
 import com.projeto.ClientRegister.entities.Client;
 import com.projeto.ClientRegister.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +21,30 @@ public class ClientResource {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Client>> findAll(){
-        List<Client> list = service.findAll();
+    public ResponseEntity<List<ClientDTO>> findAllClients(){
+        List<ClientDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Client> findById(@PathVariable Long id) {
-        Client client = service.findById(id);
-        return ResponseEntity.ok().body(client);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findClientById(@PathVariable Long id) {
+        ClientDTO clientDTO = service.findById(id);
+        return ResponseEntity.ok().body(clientDTO);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Client> insert(@RequestBody Client client) {
-        client = service.insert(client);
-        return ResponseEntity.ok().body(client);
+    @PostMapping(value = "/save")
+    public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO clientDTO) {
+        clientDTO = service.insert(clientDTO);
+        return ResponseEntity.ok().body(clientDTO);
     }
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Client> update(@PathVariable Long id,@Validated @RequestBody Client client) {
-        client = service.update(id, client);
-        return ResponseEntity.ok().body(client);
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id,@Valid @RequestBody ClientDTO clientDTO) {
+        clientDTO = service.update(id, clientDTO);
+        return ResponseEntity.ok().body(clientDTO);
     }
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
