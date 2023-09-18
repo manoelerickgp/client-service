ab576c9abf9fef02babdd2eeccdd3e8f069bc64a
manoelerickgp <manoel.computacao@gmail.com>
2023-09-18 00:38:53 -0300

    Atualizando metodos do ClientService com Padrao Dto

--- src/main/java/com/projeto/ClientRegister/services/ClientService.java
+++ src/main/java/com/projeto/ClientRegister/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.projeto.ClientRegister.services;
 
+import com.projeto.ClientRegister.DTO.ClientDTO;
 import com.projeto.ClientRegister.entities.Client;
 import com.projeto.ClientRegister.repositories.ClientRepository;
 import org.springframework.http.HttpStatus;
@@ -17,30 +18,36 @@ public class ClientService {
         this.repository = repository;
     }
 
-    public List<Client> findAll(){
-        return repository.findAll();
+    public List<ClientDTO> findAll(){
+        List<Client> list = repository.findAll();
+        return list.stream().map(c -> new ClientDTO(c)).toList();
     }
 
-    public Client findById(Long id) {
-        return repository.findById(id)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id Not Found"));
+        return new ClientDTO(client);
     }
 
-    public Client insert(Client client) {
-        return repository.save(client);
+    public ClientDTO insert(ClientDTO clientDtoToBeSaved) {
+        Client clientSaved = new Client();
+        copyClientDtoToEntity(clientSaved, clientDtoToBeSaved);
+        clientSaved = repository.save(clientSaved);
+        return new ClientDTO(clientSaved);
     }
 
-    public Client update(Long id, Client clientToBeUpdated) {
-        Client clientSaved = findById(id);
-        updateClient(clientSaved, clientToBeUpdated);
-        return repository.save(clientSaved);
+    public ClientDTO update(Long id, ClientDTO clientToBeUpdated) {
+        Client clientSaved = repository.getReferenceById(id);
+        copyClientDtoToEntity(clientSaved, clientToBeUpdated);
+        clientSaved = repository.save(clientSaved);
+        return new ClientDTO(clientSaved);
     }
 
     public void delete(Long id) {
-        repository.delete(findById(id));
+        repository.deleteById(id);
     }
 
-    private void updateClient(Client clientSaved, Client clientToBeUpdated) {
+    private void copyClientDtoToEntity(Client clientSaved, ClientDTO clientToBeUpdated) {
         clientSaved.setName(clientToBeUpdated.getName());
         clientSaved.setCpf(clientToBeUpdated.getCpf());
         clientSaved.setIncome(clientToBeUpdated.getIncome());
